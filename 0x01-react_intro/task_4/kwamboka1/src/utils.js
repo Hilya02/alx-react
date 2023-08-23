0367a270269f430a41c79bfd1781db0bf45f4d95
Hilya Ntinda Tuyakula <113698301+Hilya02@users.noreply.github.com>
2023-08-23 15:30:40 +0200

    utils.js

--- 0x01-react_intro/task_4/kwamboka1/src/utils.js
+++ 0x01-react_intro/task_4/kwamboka1/src/utils.js
@@ -3,3 +3,8 @@ export const getFullYear = () => new Date().getFullYear();
 
 /* footer-copy */
 export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holberton School main dashboard");
+
+/* notifications */
+export const getLatestNotification = () => {
+	return "<strong>Urgent requirement</strong> - complete by EOD";
+};
