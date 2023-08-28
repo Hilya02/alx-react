5582e8c3fb81ddcbfa6e1ebb6ddc911b28f3cd4b
Hilya Ntinda Tuyakula <113698301+Hilya02@users.noreply.github.com>
2023-08-28 15:07:36 +0200

    Notifications.js

--- 0x02-react_props/task_4/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_4/dashboard/src/Notifications/Notifications.js
@@ -1,30 +1,50 @@
 import React from 'react'
 import close_icon from '../assets/close-icon.png'
 import { getLatestNotification } from '../utils/utils'
+import NotificationItem from './NotificationItem'
+import propTypes from 'prop-types'
 
 import './Notifications.css'
 
-export default function Notification(props) {
-  return (
-	<div className="Notifications">
-	  <button style={{ 
-		  position: 'absolute',
-		  background: 'transparent',
-		  border: 'none',
-		  right: '20px',
-		 }} 
-		 aria-label='close' 
-		 onClick={() => {
-		  console.log('Close button has been clicked');
-	  }}>
-	    <img src={close_icon} alt="close" height="15px" width="15px"></img>
-	  </button>
-	  <p>Here is the list of notifications</p>
-	  <ul>
-		  <li data-priority="default">New course available</li>
-		  <li data-priority="urgent">New resume available</li>
-	  	  <li dangerouslySetInnerHTML={getLatestNotification()}></li>
-	  </ul>
-	</div>
-  )
+const Notification = ({ displayDrawer }) => {
+	return (
+		<>
+			<div className="menuItem">
+				<p>Your notifications</p>
+			</div>
+			{displayDrawer && (
+				<div className="Notifications">
+					<button style={{
+						position: 'absolute',
+						background: 'transparent',
+						border: 'none',
+						right: '20px',
+					}}
+						aria-label='close'
+						onClick={() => {
+							console.log('Close button has been clicked');
+						}}>
+						<img src={close_icon} alt="close" height="15px" width="15px"></img>
+					</button>
+					<p>Here is the list of notifications</p>
+					<ul>
+						<NotificationItem type="default" value="New course available" />
+						<NotificationItem type="urgent" value="New resume available" />
+						<NotificationItem type="urgent" html={{ __html: getLatestNotification() }} />
+					</ul>
+				</div>
+			)}
+		</>
+	)
 }
+
+
+Notification.defaultProps = {
+	displayDrawer: false
+}
+
+Notification.propTypes = {
+	displayDrawer: propTypes.bool
+}
+
+export default Notification
