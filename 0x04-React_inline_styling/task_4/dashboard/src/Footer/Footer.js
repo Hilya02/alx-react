890cb15f1691224dc1fd1be7241d2ce4a56fd28f
Hilya Ntinda Tuyakula <113698301+Hilya02@users.noreply.github.com>
2023-09-05 11:03:17 +0200

    Update Footer.js

--- 0x04-React_inline_styling/task_4/dashboard/src/Footer/Footer.js
+++ 0x04-React_inline_styling/task_4/dashboard/src/Footer/Footer.js
@@ -1,11 +1,13 @@
+import { getFooterCopy, getFullYear } from '../utils/utils';
 import React from 'react'
-import { getFullYear, getFooterCopy } from '../utils/utils'
 import './Footer.css'
 
-export default function Footer(props) {
-	return (
-		<footer className="App-footer">
-			<p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-		</footer>
-	)
+const Footer = () => {
+  return (
+    <>
+          <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+    </>
+  )
 }
+
+export default Footer
