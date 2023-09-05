3b4aaa2cb7923d477b852c63871ca1a5826390bd
Hilya Ntinda Tuyakula <113698301+Hilya02@users.noreply.github.com>
2023-09-05 11:36:26 +0200

    Login.js

--- 0x04-React_inline_styling/task_4/dashboard/src/Login/Login.js
+++ 0x04-React_inline_styling/task_4/dashboard/src/Login/Login.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react'
-import './Login.css'
+import React from 'react'
+import { StyleSheet, css } from 'aphrodite';
 
-export default class App extends Component {
-	render() {
-		return (
-			<React.Fragment>
-				<div className="App">
-					<body className="App-body">
-						<p>Login to access the full dashboard</p>
-						<div className="inputs">
-							<label htmlFor="email" onClick={() => {
-								// select corresponding input
-								document.getElementById('password').focus();
-							}}>Email</label>
-							<input type="email" id="email" />
-							<label htmlFor="password" onClick={() => {
-								// select corresponding input
-								document.getElementById('password').focus();
-							}}>Password</label>
-							<input type="password" id="password" />
-							<button>OK</button>
-						</div>
-					</body>
-				</div>
-			</React.Fragment>
-		)
-	}
+const styles = StyleSheet.create({
+  styleMargin: {
+    margin: "0 auto",
+  },
+  small: {
+    '@media (max-width: 900px)': {
+      display: "block"
+    }
+  }
+})
+
+const Login = () => {
+  return (
+    <React.Fragment>
+        <p>Login to access the full dashboard</p>
+        <label htmlFor='email'>Email: </label>
+        <input type="email" id='email'  className={css(styles.small)} />
+        <label htmlFor='pass'>Password: </label>
+        <input type="password" id='pass' className={css(styles.small)} />
+        <button>OK</button>
+    </React.Fragment>
+  )
 }
+
+export default Login
