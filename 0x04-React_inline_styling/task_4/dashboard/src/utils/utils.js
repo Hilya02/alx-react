bc368467d130e86dc487de68bf7ba5e3e2e90f69
Hilya Ntinda Tuyakula <113698301+Hilya02@users.noreply.github.com>
2023-09-05 11:42:52 +0200

    utils.js

--- 0x04-React_inline_styling/task_4/dashboard/src/utils/utils.js
+++ 0x04-React_inline_styling/task_4/dashboard/src/utils/utils.js
@@ -1,17 +1,5 @@
-function getFullYear() {
-	  return new Date().getFullYear();
-}
+export const getFooterCopy = (isIndex) => isIndex ? "Holberton School" : "Holberton School main dashboard"
 
-function getFooterCopy(isIndex) {
-	if (!isIndex) return "Holberton School main dashboard";
-	return "Holberton School";
-}
+export const getFullYear = () => new Date().getFullYear()
 
-function getLatestNotification() {
-	return {
-		__html: '<strong>Urgent requirement</strong> - complete by EOD'
-	}
-}
-
-// export
-export { getFullYear, getFooterCopy, getLatestNotification };
+export const getLatestNotification = () => "<strong>Urgent requirement</strong> - complete by EOD"
