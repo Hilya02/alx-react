c45e0a1b4b9772624bf277f2c5c094cb9f0f2a23
Hilya Ntinda Tuyakula <113698301+Hilya02@users.noreply.github.com>
2023-09-05 10:21:30 +0200

    Update webpack.config.js

--- 0x04-React_inline_styling/task_4/dashboard/config/webpack.config.js
+++ 0x04-React_inline_styling/task_4/dashboard/config/webpack.config.js
@@ -1,48 +1,65 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	mode: 'development',
-	entry: './src/index.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve('./dist')
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.(png|jpe?g|gif)$/i,
-				use: [
-					'file-loader',
-					{
-						loader: 'image-webpack-loader',
-						options: {
-							bypassOnDebug: true,
-							disable: true
-						},
-					},
-				],
-			},
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env', '@babel/preset-react']
-					}
-				}
-			}
-		]
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		static: path.resolve('./dist'),
-		compress: true,
-		port: 8564,
-	},
-};
+    mode:  "development",
+    entry: path.join(__dirname, '../src/index.js'),
+    output: {
+        path: path.join(__dirname, '../dist'),
+        filename: 'bundle.js'
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
+    devtool: "inline-source-map",
+    stats: {
+        colors: true,
+        modules: true,
+        reasons: true,
+        errorDetails: true,
+        children: true
+      },
+      devServer: {
+        static: {
+          directory: path.join(__dirname, '../dist'),
+        },
+        hot: true,
+        port: 8564
+      },
+    module: {
+        rules: [
+            {
+                test:/\.(js|jsx)$/i,
+                exclude:/node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ],
+            }
+        ]
+    },
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //         template: path.join(__dirname, '../src/index.html'),
+    //         favicon: path.join(__dirname, '../src/assets/favicon.ico')
+    //     })
+    // ]
+}
