924be3b3b1ecb0d513defd9961cdb79c72cd5caa
Hilya Ntinda Tuyakula <113698301+Hilya02@users.noreply.github.com>
2023-09-05 10:55:02 +0200

    BodySectionWithMarginBottom.js

--- 0x04-React_inline_styling/task_4/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x04-React_inline_styling/task_4/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,32 +1,27 @@
-import React, { Component } from 'react';
-import propTypes from 'prop-types';
-import BodySection from './BodySection';
-import './BodySectionWithMarginBottom.css';
+import React, { Component } from 'react'
+import BodySection from './BodySection'
+import PropTypes from 'prop-types'
+import { StyleSheet, css } from 'aphrodite'
 
+const styles = StyleSheet.create({
+  bodySectionWithMargin: {
+    marginBottom: 40
+  }
+})
 
-class BodySectionWithMarginBottom extends Component {
-	render() {
-		return (
-			<div className="bodySectionWithMargin">
-				{/* calls BodySection with all props */}
-				{/* "title" prop will be in h2, all else in following p tag */}
-				<BodySection {...this.props} />
-			</div>
-		)
-	}
+export class BodySectionWithMarginBottom extends Component {
+  render() {
+    return (
+      <div className={css(styles.bodySectionWithMargin)}>
+        <BodySection {...this.props} />
+      </div>
+    )
+  }
 }
-
 
 BodySectionWithMarginBottom.propTypes = {
-	title: propTypes.string.isRequired,
-	children: propTypes.oneOfType([
-		propTypes.string,
-		propTypes.element
-	])
+    title: PropTypes.string,
+    children: PropTypes.node
 }
 
-BodySectionWithMarginBottom.defaultProps = {
-	children: <React.Fragment />
-}
-
-export default BodySectionWithMarginBottom;
+export default BodySectionWithMarginBottom
