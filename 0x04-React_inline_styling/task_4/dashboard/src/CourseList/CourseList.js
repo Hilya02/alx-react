fb0469b321f333c996078b6e199c422fcae68128
Hilya Ntinda Tuyakula <113698301+Hilya02@users.noreply.github.com>
2023-09-05 10:57:43 +0200

    CourseList.js

--- 0x04-React_inline_styling/task_4/dashboard/src/CourseList/CourseList.js
+++ 0x04-React_inline_styling/task_4/dashboard/src/CourseList/CourseList.js
@@ -1,22 +1,45 @@
 import React from 'react'
 import CourseListRow from './CourseListRow'
-import './CourseList.css'
+import PropTypes from 'prop-types'
+import CourseShape from './CourseShape'
+import { StyleSheet, css } from 'aphrodite';
 
+const styles = StyleSheet.create({
+  table: {
+    border: "solid 2px rgb(189, 187, 187)",
+    width: "100%",
+    textAlign: "start"
+  },
+  thead: {
+    borderBottom: "solid 2px rgb(189, 187, 187)",
+    textAlign: "start"
+  },
+})
 
-const CourseList = ({ }) => {
-	return (
-		<table id="CourseList">
-			<thead>
-				<CourseListRow isHeader={true} textFirstCell="Available Courses" />
-				<CourseListRow isHeader={true} textFirstCell="Course name" textSecondCell="Credit" />
-			</thead>
-			<tbody id="CourseBody">
-				<CourseListRow isHeader={false} textFirstCell="ES6" textSecondCell="60" />
-				<CourseListRow isHeader={false} textFirstCell="Webpack" textSecondCell="20" />
-				<CourseListRow isHeader={false} textFirstCell="React" textSecondCell="40" />
-			</tbody>
-		</table>
-	)
+const CourseList = ({ listCourses }) => {
+  return (
+    <table id="CourseList" className={css([styles.table, styles.th, styles.tr])}>
+        <thead className={css(styles.thead)}>
+            <CourseListRow isHeader={true} textFirstCell="Available courses"/>
+            <CourseListRow isHeader={true} textFirstCell="Course name" textSecondCell="Credit"/>
+        </thead>
+        <tbody>
+        {listCourses.length === 0  ? 'No course available yet' : 
+          listCourses.map((courses) => (
+            <CourseListRow key={courses.id}  isHeader={false} textFirstCell={courses.name} textSecondCell={courses.credit}/>
+          ))
+        }
+        </tbody>
+    </table>
+  )
+}
+
+CourseList.defaultProps = {
+  listCourses: []
+}
+
+CourseList.propTypes = {
+  listCourses: PropTypes.arrayOf(CourseShape)
 }
 
 
