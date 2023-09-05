9d89c5dffec6518b7c7951d701730d00c0617441
Hilya Ntinda Tuyakula <113698301+Hilya02@users.noreply.github.com>
2023-09-05 11:37:25 +0200

    Login.test.js

--- 0x04-React_inline_styling/task_4/dashboard/src/Login/Login.test.js
+++ 0x04-React_inline_styling/task_4/dashboard/src/Login/Login.test.js
@@ -1,18 +1,17 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import Login from './Login';
+import {StyleSheetTestUtils} from 'aphrodite';
 
+StyleSheetTestUtils.suppressStyleInjection();
 
-// shallow render login component
-describe('<Login />', () => {
-	it('Tests that Login renders without crashing', () => {
-		const wrapper = shallow(<Login />);
-		expect(wrapper.exists()).toBe(true);
-	})
-
-	it('Tests that the component renders 2 <input> and 2 <label> tags', () => {
-		const wrapper = shallow(<Login />);
-		expect(wrapper.find('input').length).toBe(2);
-		expect(wrapper.find('label').length).toBe(2);
-	})
-});
+describe("Test Login Component", () => {
+    test("renders without crashing", () => {
+        const wrapper = shallow(<Login />)
+    })
+    test("renders 2 input tags and 2 label tags", () => {
+        const wrapper = shallow(<Login />)
+        expect(wrapper.find('input').length).toBe(2)
+        expect(wrapper.find('label').length).toBe(2)
+    })
+})
