d8f98a05d1c29e314236ad7d01bc6b5aafa18cfe
Hilya Ntinda Tuyakula <113698301+Hilya02@users.noreply.github.com>
2023-09-05 11:01:01 +0200

    Update CourseListRow.test.js

--- 0x04-React_inline_styling/task_4/dashboard/src/CourseList/CourseListRow.test.js
+++ 0x04-React_inline_styling/task_4/dashboard/src/CourseList/CourseListRow.test.js
@@ -1,24 +1,21 @@
-import { shallow } from 'enzyme';
-import React from 'react';
-import CourseListRow from './CourseListRow';
+import React from "react";
+import Enzyme, { shallow } from 'enzyme'
+import CourseListRow from "./CourseListRow";
+import {StyleSheetTestUtils} from 'aphrodite';
 
+StyleSheetTestUtils.suppressStyleInjection();
 
-// shallow render CourseListRow component
-describe('<CourseListRow />', () => {
-	it(`When isHeader is true, renders on cell with colspan=2
-	when textSecondCell does not exist`, () => {
-		const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" />);
-		expect(wrapper.find('th').exists()).toBe(true);
-	})
+describe("Test CourseListRow component", () => {
+    test("renders one cell with colspan = 2 when textSecondCell does not exist and header is true", () => {
+        const wrapper = shallow(<CourseListRow  isHeader={true} textFirstCell="First cell"/>)
+        expect(wrapper.find('th').length).toBe(1)
+        expect(wrapper.find('th').text()).toBe("First cell")
 
-	it(`When isHeader is true, renders two cells when textSecondCell is present`, () => {
-		const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" textSecondCell="test2" />);
-		expect(wrapper.find('th').length).toBe(2);
-	})
-
-	it(`When isHeader is false, renders two td elements within a tr element`, () => {
-		const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="test" textSecondCell="test2" />);
-		expect(wrapper.find('tr').length).toBe(1);
-		expect(wrapper.find('td').length).toBe(2);
-	})
+    })
+    test("renders two cells when textSecondCell is present", () => {
+        const wrapper = shallow(<CourseListRow  isHeader={true} textFirstCell="First cell" textSecondCell="Second cell" />)
+        expect(wrapper.find('th').length).toBe(2)
+        expect(wrapper.find('th').at(0).text()).toBe("First cell")
+        expect(wrapper.find('th').at(1).text()).toBe("Second cell")
+    })
 })
