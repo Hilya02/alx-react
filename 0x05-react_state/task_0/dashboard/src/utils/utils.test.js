b99e70cb024658f01099af885c19e10e6514b4ba
Hilya Ntinda Tuyakula <113698301+Hilya02@users.noreply.github.com>
2023-09-11 16:37:46 +0200

    Update utils.test.js

--- 0x05-react_state/task_0/dashboard/src/utils/utils.test.js
+++ 0x05-react_state/task_0/dashboard/src/utils/utils.test.js
@@ -1,22 +1,50 @@
-import React from 'react';
-import Adapter from 'enzyme-adapter-react-16';
-import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
+/**
+ * @jest-environment jsdom
+ */
 
-test('Checking Year', () => {
-    expect(getFullYear()).toEqual(2022)
-})
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import { getFooterCopy, getFullYear, getLatestNotification } from "./utils";
 
-test('checking footerCopy when input true', () =>  {
-    expect(getFooterCopy(true)).toEqual("Holberton School")
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
 
-})
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
 
-test('checking footerCopy when input false', () =>  {
-    expect(getFooterCopy(false)).toEqual("Holberton School main dashboard")
+it("renders the year 2021", () => {
+  act(() => {
+    render(getFullYear(), container);
+  });
+  expect(container.textContent).toBe("2021");
+});
 
-})
+it("checks getFooterCopy returns the correct string when true", () => {
+  act(() => {
+    render(getFooterCopy(true), container);
+  });
+  expect(container.textContent).toBe("Holberton School");
+});
 
-test('checking notificatiocatio', () =>  {
-    expect(getLatestNotification(false)).toEqual("<strong>Urgent requirement</strong> - complete by EOD")
+it("checks getFooterCopy returns the correct string when false", () => {
+  act(() => {
+    render(getFooterCopy(false), container);
+  });
+  expect(container.textContent).toBe("Holberton School main dashboard");
+});
 
-})
+it("checks getLatestNotification returns the correct string", () => {
+  act(() => {
+    render(getLatestNotification(), container);
+  });
+  expect(container.textContent).toBe("<strong>Urgent requirement</strong> - complete by EOD");
+});
