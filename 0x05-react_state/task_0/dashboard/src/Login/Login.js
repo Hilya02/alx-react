83a8de014267f56583c159230bd57cfae271e900
Hilya Ntinda Tuyakula <113698301+Hilya02@users.noreply.github.com>
2023-09-11 16:27:58 +0200

    Login.js

--- 0x05-react_state/task_0/dashboard/src/Login/Login.js
+++ 0x05-react_state/task_0/dashboard/src/Login/Login.js
@@ -1,28 +1,47 @@
-import React from 'react'
+import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
 
+const Login = () => {
+  return (
+    <>
+      <div className={css(styles.appBody, styles.small)}>
+        <h1>Log in to continue</h1>
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email: </label>
+        <input className={css(styles.noBorder)} type="email" id="email" name="email" />
+        <label htmlFor="password">Password: </label>
+        <input className={css(styles.noBorder)} type="password" id="password" name="password" />
+        <button className={css(styles.yellowBorder)}>OK</button>
+      </div>
+    </>
+  );
+}
+
 const styles = StyleSheet.create({
-  styleMargin: {
-    margin: "0 auto",
+  appBody: {
+    minHeight: '50vh',
+    textAlign: 'left',
+    marginTop: '2rem',
+    marginLeft: '2rem',
   },
   small: {
     '@media (max-width: 900px)': {
-      display: "block"
+      display: 'grid',
+      justifyContent: 'center',
+    }
+  },
+  noBorder: {
+    '@media (max-width: 900px)': {
+      border: 'none'
+    }
+  },
+  yellowBorder: {
+    '@media (max-width: 900px)': {
+      border: '2px solid gold',
+      backgroundColor: 'transparent',
+      width: '5vw'
     }
   }
 })
 
-const Login = () => {
-  return (
-    <React.Fragment>
-        <p>Login to access the full dashboard</p>
-        <label htmlFor='email'>Email: </label>
-        <input type="email" id='email'  className={css(styles.small)} />
-        <label htmlFor='pass'>Password: </label>
-        <input type="password" id='pass' className={css(styles.small)} />
-        <button>OK</button>
-    </React.Fragment>
-  )
-}
-
-export default Login
+export default Login;
