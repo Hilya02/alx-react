4c318f88c7a83a03eaf81bab2ef713962e0fe324
Hilya Ntinda Tuyakula <113698301+Hilya02@users.noreply.github.com>
2023-09-11 16:06:25 +0200

    BodySection.test.js

--- 0x05-react_state/task_0/dashboard/src/BodySection/BodySection.test.js
+++ 0x05-react_state/task_0/dashboard/src/BodySection/BodySection.test.js
@@ -1,22 +1,26 @@
-import React from "react";
-import { shallow, mount } from "enzyme";
-import BodySection from './BodySection'
-import {StyleSheetTestUtils} from 'aphrodite';
+import React from 'react';
+import { StyleSheetTestUtils } from 'aphrodite';
+import { shallow } from 'enzyme';
+import BodySection from './BodySection';
 
-StyleSheetTestUtils.suppressStyleInjection();
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
 
-describe("Testing BodySection", () => {
-    let wrapper;
-    beforeEach(() => {
-        wrapper = shallow(
-        <BodySection title="test title">
-            <p>test children node</p>
-        </BodySection>)
-    })
-    test("checking that shallowing the component should render correctly the children and one h2 element", () => {
-        expect(wrapper.find('h2').length).toBe(1)
-        expect(wrapper.find('h2').text()).toBe('test title')
-        expect(wrapper.find('p').length).toBe(1)
-        expect(wrapper.find('p').text()).toBe('test children node')
-    })
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
+describe('<BodySection />', () => {
+  it('renders an <h2> element and children', () => {
+    const wrapper = shallow(
+      <BodySection title="test title">
+        <p>test children node</p>
+      </BodySection>);
+
+    expect(wrapper.find('h2')).toHaveLength(1);
+    expect(wrapper.find('h2').text()).toBe('test title');
+    expect(wrapper.find('p')).toHaveLength(1);
+    expect(wrapper.find('p').text()).toBe('test children node');
+  });
 })
