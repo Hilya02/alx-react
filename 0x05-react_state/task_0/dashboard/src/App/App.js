f63b86604382933d1f89522a07ef617691bf64e1
Hilya Ntinda Tuyakula <113698301+Hilya02@users.noreply.github.com>
2023-09-11 15:54:58 +0200

    App.js

--- 0x05-react_state/task_0/dashboard/src/App/App.js
+++ 0x05-react_state/task_0/dashboard/src/App/App.js
@@ -1,106 +1,124 @@
-import React, {Component } from 'react';
-import PropTypes from 'prop-types';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Login from '../Login/Login';
-import Notifications from '../Notifications/Notifications';
-import CourseList from '../CourseList/CourseList'
-import { getLatestNotification } from '../utils/utils';
-import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
-import BodySection from '../BodySection/BodySection';
+import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
+import BodySection from '../BodySection/BodySection';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import Notifications from '../Notifications/Notifications';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import CourseList from '../CourseList/CourseList';
+import PropTypes from 'prop-types';
+import { getLatestNotification } from '../utils/utils';
 
-const styles = StyleSheet.create({
-  App: {
-    display: 'grid',
-    gridTemplateRows: "1fr 1fr",
-  },
-  AppBody: {
-    paddingLeft: "3rem",
-    paddingTop: "3rem",
-  },
-  footer: {
-    borderTop: "4px solid red",
-    paddingTop: "1rem"
-  },
-  Footerp: {
-      textAlign: "center"
-    },
-    small: {
-      '@media (max-width: 900px)': {
-        display: "none"
-      }
-    }
-})
+class App extends React.Component {
+	constructor(props) {
+		super(props);
 
-const listCourses = [
-  {id: 1, name: "ES6", credit: 60},
-  {id: 2, name: "Webpack", credit: 20},
-  {id: 3, name: "React", credit: 40}
-]
-const listNotifications = [
-  {id: 1, type:'default', value:"New course available"},
-  {id: 2, type:'urgent', value:"New resume available"},
-  {id: 3, type:'urgent', html:{__html: getLatestNotification() }}
-]
+		this.state = { displayDrawer: false };
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-  }
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyPress)
-  }
-  handleKeyPress = (e) => {
-    if (e.ctrlKey && e.key === 'h') {
-      alert("Logging you out");
-      this.props.logOut()
-    }
-  }
+		this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+		this.handleHideDrawer = this.handleHideDrawer.bind(this);
+	}
 
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeyPress)
-  }
-  render () {
-    return (
-      <React.Fragment>
-        <Notifications listNotifications={listNotifications} />
-        <div className={css([styles.App, styles.small])}>
-            <Header />
-            <div className={css(styles.AppBody)}>
-              {this.props.isLoggedIn ?
-              <>
-              <BodySection title="News from the School">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, odit.</p>
-              </BodySection>
-              <BodySectionWithMarginBottom title="Course list">
-                <CourseList listCourses={listCourses} />
-              </BodySectionWithMarginBottom>
-              </>
-               : 
-               <BodySectionWithMarginBottom title="Log in to continue">
-                <Login />
-              </BodySectionWithMarginBottom>
-              }
-            </div>
-        </div>
-        <div className={css([styles.footer, styles.Footerp, styles.small])}>
-          <Footer />
-        </div>
-      </React.Fragment>
-    );
-  }
-}
+	listCourses = [
+		{ id: 1, name: 'ES6', credit: 60 },
+		{ id: 2, name: 'Webpack', credit: 20 },
+		{ id: 3, name: 'React', credit: 40 },
+	];
 
-App.defaultProps = {
-  isLoggedIn: false,
-  logOut: () => {return }
+	listNotifications = [
+		{ id: 1, type: 'default', value: 'New course available' },
+		{ id: 2, type: 'urgent', value: 'New resume available' },
+		{ id: 3, type: 'default', html: getLatestNotification() },
+	];
+
+	componentDidMount() {
+		document.addEventListener('keydown', (e) => {
+			if (e.ctrlKey && e.key === 'h') {
+				alert('Logging you out');
+				this.props.logOut();
+			}
+		});
+	}
+
+	componentWillUnmount() {
+		document.removeEventListener('keydown', (e) => {
+			if (e.ctrlKey && e.key === 'h') {
+				alert('Logging you out');
+				this.props.logOut();
+			}
+		});
+	}
+
+	handleDisplayDrawer() {
+		this.setState({ displayDrawer: true });
+	}
+
+	handleHideDrawer() {
+		this.setState({ displayDrawer: false });
+	}
+
+	render() {
+		return (
+			<>
+				<div className={css(styles.container, styles.small)}>
+					<Header />
+					<Notifications
+						listNotifications={this.listNotifications}
+						displayDrawer={this.state.displayDrawer}
+						handleDisplayDrawer={this.handleDisplayDrawer}
+						handleHideDrawer={this.handleHideDrawer}
+					/>
+				</div>
+				<hr className={css(styles.hr)} />
+				{this.props.isLoggedIn ? (
+					<BodySectionWithMarginBottom>
+						<CourseList listCourses={this.listCourses} />
+					</BodySectionWithMarginBottom>
+				) : (
+					<BodySectionWithMarginBottom>
+						<Login />
+					</BodySectionWithMarginBottom>
+				)}
+				<BodySection title='News from the School'>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua.{' '}
+					</p>
+				</BodySection>
+				<hr className={css(styles.hr)} />
+				<Footer />
+			</>
+		);
+	}
 }
 
 App.propTypes = {
-  isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func
-}
+	isLoggedIn: PropTypes.bool,
+	logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+	isLoggedIn: false,
+	logOut: () => {
+		return;
+	},
+};
+
+const styles = StyleSheet.create({
+	container: {
+		display: 'flex',
+		justifyContent: 'space-between',
+	},
+	hr: {
+		borderTop: '2px solid red',
+	},
+	small: {
+		'@media (max-width: 900px)': {
+			display: 'grid',
+			justifyContent: 'center',
+		},
+	},
+});
 
 export default App;
