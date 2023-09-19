52adcbc59967395e25ce0aadf17ee01aa638163c
Hilya Ntinda Tuyakula <113698301+Hilya02@users.noreply.github.com>
2023-09-19 10:22:32 +0200

    Footer.js

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/Footer/Footer.js
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/Footer/Footer.js
@@ -1,13 +1,36 @@
-import { getFooterCopy, getFullYear } from '../utils/utils';
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import { getFullYear, getFooterCopy } from "../utils/utils";
+import { StyleSheet, css } from "aphrodite";
+import { AppContext } from "../App/AppContext";
 
-const Footer = () => {
+
+const styles = StyleSheet.create({
+  paragraph: {
+    textAlign: 'center',
+    fontSize: '1.2rem'
+  },
+  copyright: {
+    marginTop: '40px'
+  }
+})
+
+function Footer() {
   return (
-    <>
-          <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-    </>
-  )
+    <AppContext.Consumer>
+      {(context) => (
+        <footer className='footer'>
+          <p className={css(styles.copyright)}>
+            Copyright {getFullYear()} - {getFooterCopy(true)}
+          </p>
+          {context.user.isLoggedIn && (
+            <p className={css(styles.paragraph)}>
+              <a>Contact us</a>
+            </p>
+          )}
+        </footer>
+      )}
+    </AppContext.Consumer>
+  );
 }
 
-export default Footer
+export default Footer;
